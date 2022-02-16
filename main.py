b5ca35c6ca09ad13a5c566c44316ccaf3bc7af77
Sualeh Asif <sualeh@mit.edu>
2022-02-16 16:56:04 -0500

    updated with slightly nicer side bar handling.

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ import json
 production = True
 
 # """
-# TODO: side by side comparison of graphs
-# TODO: choose which graphs to view with radio buttons
+# DONE: side by side comparison of graphs
+# DONE: choose which graphs to view with radio buttons
 # TODO: link sharing
 # TODO: put data per graph
 # """
 
 data_directory = "compressed-data"
 config_name = "graph_config.json"
+
 graph_config = json.load(open(config_name))
 st.set_page_config(layout="wide")
+
+all_fields = [
+    "num_nodes", "num_edges", "average_degree", "max_degree", "num_triangles",
+    "new_edges"
+]
 
 graph_names = {}
 for key in graph_config:
@@ -25,42 +31,53 @@ for key in graph_config:
     graph_names[name] = key
 
 # We start with a select box to choose what goes on the side.
-graph_selected = st.sidebar.selectbox(
-    "Which graph would you like to view?", tuple(graph_names.keys())
-)
 
-side_by_side = st.sidebar.checkbox("View side by side")
+side_by_side = st.sidebar.checkbox("Open a side by side comparison",
+                                   value=False)
 
-if side_by_side:
+if not side_by_side:
+    graph_selected = st.sidebar.selectbox(
+        "Which graph would you like to view?", tuple(graph_names.keys()))
+else:
     graph_selected = 0
     side_graph_1 = st.sidebar.selectbox(
-        "Which graph would you like to view on the left?", tuple(graph_names.keys())
-    )
+        "Which graph would you like to view on the left?",
+        tuple(graph_names.keys()))
     side_graph_2 = st.sidebar.selectbox(
-        "Which graph would you like to view on the right?", tuple(graph_names.keys())
-    )
+        "Which graph would you like to view on the right?",
+        tuple(graph_names.keys()))
 
-    fields = ["num_nodes", "num_edges", "average_degree", "max_degree", "num_triangles", "new_edges"]
+    fields = all_fields.copy()
 
-    sidebar_prop = st.sidebar.selectbox(
-        "Which property would you like to view?", tuple(fields)
-    )
+    props_to_display = [False] * len(fields)
 
+    st.sidebar.markdown("Which properties would you like to view?")
+    for i, field in enumerate(fields):
+        props_to_display[i] = st.sidebar.checkbox(field)
 
 # TODO: Add a slider later for how much we should subsample?
 
-def get_data(graph_name, col=None, config=graph_config, prop=None):
-    name = "compressed-" + config[graph_name]["name"]
-    dir_name = os.path.join(data_directory, config[graph_name]["directory"])
-    
-    files = [f for f in os.listdir(dir_name)]
-    fields = ["num_nodes", "num_edges", "average_degree", "max_degree", "num_triangles", "new_edges"]
 
+def get_data(graph_name, col=None, config=graph_config, props_selected=None):
+    dir_name = os.path.join(data_directory, config[graph_name]["directory"])
+
+    files = [f for f in os.listdir(dir_name)]
+
+    fields = all_fields.copy()
+
+    # overwrite with props if necessary
+    new_fields = []
+    if props_selected is not None:
+        for i, prop in enumerate(props_selected):
+            if prop:
+                new_fields.append(fields[i])
+        if len(new_fields) != 0:
+            fields = new_fields
+
+    # dont display many fields if not in production.
     if not production:
         files = files[:4]
 
-    if prop is not None:
-        fields = [prop]
     dfs = []
     for fname in files:
         df_file = os.path.join(dir_name, fname)
@@ -91,13 +108,11 @@ def get_data(graph_name, col=None, config=graph_config, prop=None):
                 st.write(f"### {field}")
                 with st.spinner(f"Calculating {field} for {name} ..."):
                     st.line_chart(df)
-
         else:
             st.write(f"### {field}")
             with st.spinner(f"Calculating {field} for {name} ..."):
                 st.line_chart(df, width=700, use_container_width=False)
 
-        
         # df.plot(x="timestep", legend=False)
         # plt.ylim(ymin=0)
         # plt.title(output_prefix.split("/")[-1]+"_"+field)
@@ -109,6 +124,7 @@ def get_data(graph_name, col=None, config=graph_config, prop=None):
 
     # data = pd.read_csv(f"{data_directory}/{dir_name}/{graph_name}.adj_stats.csv.0")
 
+
 # @st.cache
 def make_line_chart(data):
     """
@@ -117,34 +133,42 @@ def make_line_chart(data):
     with st.spinner("Making the line graph slowly ..."):
         st.line_chart(data)
 
-def graph_displayer(graph_name, col=None, config=graph_config, prop=None):
-    name = config[graph_name]["name"]
 
-    st.title(f"{name} Graph")
-    
+def graph_displayer(graph_name,
+                    col=None,
+                    config=graph_config,
+                    props_selected=None):
+
     # check if the description exists and if so display it
     if "description" in config[graph_name]:
         st.write(config[graph_name]["description"])
-    
-    get_data(graph_name, col, config, prop)
+
+    get_data(graph_name, col, config, props_selected=props_selected)
 
     # st.write(data.head())
     # st.write(data.describe())
 
     # make_line_chart(data)
 
+
 if graph_selected:
     graph_to_display = graph_names[graph_selected]
+    title = graph_config[graph_to_display]["name"]
+    st.title(f"{title} Graph")
+
     graph_displayer(graph_to_display)
 
 if side_by_side:
-    
     col1, col2 = st.columns(2)
-    col1.title(f"{graph_names[side_graph_1]} Graph")
-    graph_left = graph_names[side_graph_1]
-    graph_displayer(graph_left, col=col1, prop=sidebar_prop)
 
-    graph_right = graph_names[side_graph_2]
-    col2.title(f"{graph_names[side_graph_2]} Graph")
-    graph_displayer(graph_right, col=col2, prop=sidebar_prop)
+    left_graph = graph_names[side_graph_1]
+    left_name = graph_config[left_graph]["name"]
+    col1.title(f"{left_name}")
 
+    graph_displayer(left_graph, col=col1, props_selected=props_to_display)
+
+    right_graph = graph_names[side_graph_2]
+    right_name = graph_config[right_graph]["name"]
+    col2.title(f"{right_name}")
+
+    graph_displayer(right_graph, col=col2, props_selected=props_to_display)
